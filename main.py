05ab1599fa3d3e563442f10747daecd23f43633d
kostya-kononenko <kkononenko3@gmail.com>
2023-01-04 09:26:53 +0200

    second_commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main_function(x):
-    if x > 0:
+    if x > 0 and x <= 10:
         print('Число більше нуля')
+    elif x > 11:
+        print('Число більше десяти')
 
 
-main_function(5)
+main_function(12)
