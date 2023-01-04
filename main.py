635e97da8498ec99fd258aabb14441a3685cfb32
kostya-kononenko <kkononenko3@gmail.com>
2023-01-04 09:53:42 +0200

    first_commit_test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def main_function(x):
-    if x > 0 and x <= 10:
-        print('Число від нуля до десяти (включно)')
-    elif x > 11 and x <= 20:
-        print('Число від одинадцяти до двадцяти (включно)')
-    elif x > 21:
-        print('Число більше двадцяти')
+def test_function(x):
+    if x > 40 and x <= 50:
+        print('Число від сорока до п\'ятдесяти (включно)')
+    elif x > 51 and x <= 60:
+        print('Число від п\'ятдесяти одного до шестидесяти (включно)')
 
 
-main_function(22)
+test_function(50)
