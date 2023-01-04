b82d9d4e71b3e5324dcbb8a74eb9552997cec34a
kostya-kononenko <kkononenko3@gmail.com>
2023-01-04 10:04:21 +0200

    first_commit_main_function_number_two

---
+++ py_charm.py
@@ -0,0 +1,8 @@
+def main_function_number_two(x):
+    if x > 100 and x <= 200:
+        print('Число від 100 до 200 (включно)')
+    elif x > 201 and x <= 300:
+        print('Число від 201 до 300 (включно)')
+
+
+main_function_number_two(200)
