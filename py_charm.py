54909eddfecd88dc0ba8340406818b441f894ebc
kostya-kononenko <kkononenko3@gmail.com>
2023-01-04 10:05:51 +0200

    third_commit_main_function_number_two

--- py_charm.py
+++ py_charm.py
@@ -5,6 +5,8 @@ def main_function_number_two(x):
         print('Число від 201 до 300 (включно)')
     elif x > 301 and x <= 400:
         print('Число від 301 до 400 (включно)')
+    elif x > 401 and x <= 500:
+        print('Число від 401 до 500 (включно)')
 
 
 
