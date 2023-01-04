7e3967e106cf9dd7a40109becefae4bb89e335ee
kostya-kononenko <kkononenko3@gmail.com>
2023-01-04 09:30:30 +0200

    third_commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main_function(x):
     if x > 0 and x <= 10:
-        print('Число більше нуля')
-    elif x > 11:
-        print('Число більше десяти')
+        print('Число від нуля до десяти (включно)')
+    elif x > 11 and x <= 20:
+        print('Число від одинадцяти до двадцяти (включно)')
+    elif x > 21:
+        print('Число більше двадцяти')
 
 
-main_function(12)
+main_function(22)
